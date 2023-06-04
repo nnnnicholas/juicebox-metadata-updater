7957d601e5eaaf47f8e43d039528aaae12d7d59c
nicholas <nnnnicholas@users.noreply.github.com>
2023-06-04 15:58:52 -0400

    update get1155TokenIds to export fn

--- src/get1155TokenIdsFromGraph.ts
+++ src/get1155TokenIdsFromGraph.ts
@@ -1,31 +1,36 @@
+// Fetches the 1155 token ids of a given contract via The Graph
 import { config } from "dotenv";
 config();
 
-const apiKey = process.env.THEGRAPH_API_KEY;
-const endpoint = `https://gateway.thegraph.com/api/${apiKey}/subgraphs/id/GCQVLurkeZrdMf4t5v5NyeWJY8pHhfE9sinjFMjLYd9C`;
+export async function get1155TokenIds() {
+  const apiKey = process.env.THEGRAPH_API_KEY;
+  const endpoint = `https://gateway.thegraph.com/api/${apiKey}/subgraphs/id/GCQVLurkeZrdMf4t5v5NyeWJY8pHhfE9sinjFMjLYd9C`;
 
-const query = `
-{
-  erc1155Tokens(where: { contract: "0xe601eae33a0109147a6f3cd5f81997233d42fedd" }) {
-    id
+  const query = `
+  {
+    erc1155Tokens(where: { contract: "0xe601eae33a0109147a6f3cd5f81997233d42fedd" }) {
+      id
+    }
   }
-}
-`;
+  `;
 
-fetch(endpoint, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-  body: JSON.stringify({
-    query,
-  }),
-})
-  .then((r) => r.json())
-  .then((data) => {
-    const ids = data.data.erc1155Tokens.map((token: { id: string }) =>
-      parseInt(token.id.split("/")[1])
-    );
-    console.log(ids);
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query,
+    }),
   });
+
+  const data = await response.json();
+  const ids = data.data.erc1155Tokens.map((token: { id: string }) =>
+    parseInt(token.id.split("/")[1])
+  );
+  return ids;
+}
+
+// For local testing
+// get1155TokenIds().then((res) => console.log("1155 token ids:", res));
